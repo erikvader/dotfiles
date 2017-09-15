a97d54aadc22627e1aef7e411e949b642f70801d
Erik Rimskog <erik.rimskog@gmail.com>
2017-09-15 15:18:53 +0200

    Added makefiles

--- zsh/custom/aliases.sh
+++ zsh/custom/aliases.sh
@@ -1,3 +1,3 @@
 alias ec="emacsclient"
-alias copycmakefile="cp /home/erik/Dropbox/programmering/c/make_test/makefile makefile"
+alias copycmakefile="cp /home/erik/Dropbox/program\ settings/makefiles/c-makefile makefile"
 alias gotoschool="cd /home/erik/Dropbox/skol-grejer/Universitet/åk\ 2"
