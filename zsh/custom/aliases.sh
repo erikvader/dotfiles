1653bfd99b560dfbcb07e1f4b91e5e9780d55452
Erik Rimskog <erik.rimskog@gmail.com>
2017-11-11 22:15:14 +0100

    added mvlink function to zsh

--- zsh/custom/aliases.sh
+++ zsh/custom/aliases.sh
@@ -1,3 +1,10 @@
 alias ec="emacsclient"
 alias copycmakefile="cp /home/erik/Dropbox/program\ settings/makefiles/c-makefile makefile"
 alias gotoschool="cd /home/erik/Dropbox/skol-grejer/Universitet/åk\ 2"
+
+#mvlink .inputrc Dropbox/
+function mvlink {
+    mv "$1" "$2"
+    ln -s "$2$1" "$1"
+}
+
