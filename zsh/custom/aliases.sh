3b92aaf6ddc031ae4f3255e8fa065801a3a53a2a
Erik Rimskog <erik.rimskog@gmail.com>
2017-11-11 23:34:56 +0100

    added alias

--- zsh/custom/aliases.sh
+++ zsh/custom/aliases.sh
@@ -1,6 +1,7 @@
 alias ec="emacsclient"
 alias copycmakefile="cp /home/erik/Dropbox/program\ settings/makefiles/c-makefile makefile"
 alias gotoschool="cd /home/erik/Dropbox/skol-grejer/Universitet/åk\ 2"
+alias nemo="nemo --no-desktop"
 
 #mvlink .inputrc Dropbox/
 function mvlink {
