725b1fbc46efa4d1d6fe0dc943249504dcf534e5
Erik Rimskog <erik.rimskog@gmail.com>
2018-01-05 01:13:30 +0100

    made lock.sh better

--- i3/lock.sh
+++ i3/lock.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-SCREEN=/tmp/screen.png
-LOCK=~/.config/i3/lock.png
+SCREEN='/tmp/screen.png'
+LOCK=~/.i3/lock.png
 
-scrot $SCREEN
-convert $SCREEN -scale 10% -scale 1000% $SCREEN
-convert $SCREEN $LOCK -gravity center -composite -matte $SCREEN
+scrot --delay 0 --quality 1 "$SCREEN"
+convert "$SCREEN" -scale 10% -scale 1000% "$SCREEN"
+convert "$SCREEN" "$LOCK" -gravity center -composite -matte "$SCREEN"
 #dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Stop
-i3lock -i $SCREEN
-rm $SCREEN
+i3lock -i "$SCREEN"
+rm "$SCREEN"
 
