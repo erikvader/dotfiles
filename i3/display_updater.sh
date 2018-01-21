57c7b2d750b88ebeaa398c2ab7143bb4c3dfebdc
Erik Rimskog <erik.rimskog@gmail.com>
2018-01-21 21:55:11 +0100

    display_updater prints more detailed message

--- i3/display_updater.sh
+++ i3/display_updater.sh
@@ -12,11 +12,12 @@ if echo "$connected" | grep "$out2" &>/dev/null; then
     out2c="1"
 fi
 
-notify-send "$connected"
 if [[ "$out2c" ]]; then
     xrandr --output "$out1" --auto --primary --output "$out2" --auto --right-of "$out1" && ~/.config/polybar/run_polybar.sh "$out1" "$out2"
+    notify-send "$connected" "$out2 activated"
 else
     xrandr --output "$out1" --auto --primary --output "$out2" --off && ~/.config/polybar/run_polybar.sh "$out1"
+    notify-send "$connected" "$out2 deactivated"
 fi
 ~/.fehbg
 
