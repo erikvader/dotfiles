15264ad2a7ce4841f1772fcd04754a96684cc913
Erik Rimskog <erik.rimskog@gmail.com>
2018-01-23 23:45:11 +0100

    made display_updater.sh simpler (and better)

--- i3/display_updater.sh
+++ i3/display_updater.sh
@@ -2,22 +2,22 @@
 
 out1='eDP1'
 out2='HDMI1'
-connected="$(xrandr | grep ' connected ' | sed -E 's/^(.+?) connected.*$/\1/' | xargs -d'\n' echo)"
+# connected="$(xrandr | grep ' connected ' | sed -E 's/^(.+?) connected.*$/\1/' | xargs -d'\n' echo)"
 
 # if echo "$connected" | grep "$out1" &>/dev/null; then
 #     out1c="1"
 # fi
 
-if echo "$connected" | grep "$out2" &>/dev/null; then
-    out2c="1"
-fi
+# if echo "$connected" | grep "$out2" &>/dev/null; then
+#     out2c="1"
+# fi
 
-if [[ "$out2c" ]]; then
+if xrandr | grep -E "^$out2 connected"; then
     xrandr --output "$out1" --auto --primary --output "$out2" --auto --right-of "$out1" && ~/.config/polybar/run_polybar.sh "$out1" "$out2"
-    notify-send "$connected" "$out2 activated"
+    notify-send "$out2 activated"
 else
     xrandr --output "$out1" --auto --primary --output "$out2" --off && ~/.config/polybar/run_polybar.sh "$out1"
-    notify-send "$connected" "$out2 deactivated"
+    notify-send "$out2 deactivated"
 fi
 ~/.fehbg
 
