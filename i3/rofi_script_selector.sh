b9a800e5a82cc4ac6a6538899587c32f9e2a5a88
Erik Rimskog <erik.rimskog@gmail.com>
2018-03-01 20:59:57 +0100

    audio powersave toggle and rofi scripts menu

---
+++ i3/rofi_script_selector.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+scripts=("xrandr-invert-colors"
+         "$HOME/.i3/blacken_screen.sh"
+         "$HOME/.i3/restore_screen.sh"
+         "pkill -USR1 '^redshift$'"
+         "$HOME/.i3/display_updater.sh"
+         "gksudo ""$HOME/.toggle_audio_powersave.sh"
+         "morc_menu"
+         "pavucontrol"
+         "terminal -e bmenu"
+         "pkill compton"
+         "compton -b"
+         "i3-scrot"
+         "i3-scrot -w"
+         "i3-scrot -s"
+         "$HOME/.i3/scrot_clipboard.sh"
+         "$HOME/.i3/scrot_clipboard.sh -u"
+         "$HOME/.i3/scrot_clipboard.sh -s"
+         "transset-df -a 0.99"
+         "$HOME/prog_mode_toggle.sh"
+         "$HOME/prog_mode_toggle.sh swetoggle"
+        )
+
+names=("Invert colors"
+       "Blacken screen"
+       "Restore screen"
+       "Toggle redshift"
+       "Display updater"
+       "Audio powersave toggle"
+       "Manjaro i3 menu"
+       "PulseAudio Volume Control"
+       "bmenu"
+       "Kill compton"
+       "Start compton"
+       "Screenshot whole screen"
+       "Screenshot window"
+       "Screenshot selection"
+       "Screenshot clipboard whole screen"
+       "Screenshot clipboard window"
+       "Screenshot clipboard selection"
+       "Set transparency to 99%"
+       "Program mode toggle"
+       "Program mode swedish toggle"
+      )
+
+# eller printf '%s\n' "${names[@]}"
+choice="$( IFS=$'\n'; echo -e "${names[*]}" | rofi -dmenu -i -p "Run" -no-custom -format i )"
+
+if [[ "$choice" ]]; then
+    echo "du valde ${names[$choice]}"
+    eval "${scripts[$choice]}" &
+else
+    echo "du valde inget"
+fi
---
+++ toggle_audio_powersave.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# toggle audio module powersave feature
+
+ps="/sys/module/snd_hda_intel/parameters/power_save"
+psc="/sys/module/snd_hda_intel/parameters/power_save_controller"
+
+if grep 1 "$ps" >/dev/null; then
+    echo 0 > "$psc"
+    echo 0 > "$ps"
+    notify-send 'powersave off'
+else
+    echo 'Y' > "$psc"
+    echo '1' > "$ps"
+    notify-send 'powersave on'
+fi
