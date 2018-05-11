3b409f2b4ce70d833826c0770bc815c7ad9f4564
Erik Rimskog <erik.rimskog@gmail.com>
2018-05-11 18:49:13 +0200

    powersave.sh bug with printing a 1

--- polybar/powersave.sh
+++ polybar/powersave.sh
@@ -4,7 +4,7 @@ function isActive {
     ps="/sys/module/snd_hda_intel/parameters/power_save"
     psc="/sys/module/snd_hda_intel/parameters/power_save_controller"
 
-    if grep '1' "$ps" || grep 'Y' "$psc"; then
+    if grep -q '1' "$ps" || grep -q 'Y' "$psc"; then
         # return 0
         echo ""
     else
@@ -14,8 +14,8 @@ function isActive {
 }
 
 trap isActive SIGUSR1
-trap 'trap "" TERM EXIT; kill 0; trap - INT TERM; kill -INT $$' INT
-trap "kill 0" EXIT
+# trap 'trap "" TERM EXIT; kill 0; trap - INT TERM; kill -INT $$' INT
+# trap "kill 0" EXIT
 
 while true; do
     isActive
