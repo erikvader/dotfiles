d67c015ace6694afc0a9af989df2d26a236df3b7
Erik Rimskog <erik.rimskog@gmail.com>
2018-05-14 00:13:13 +0200

    reworked prog_mode_toggle.sh

--- prog_mode_toggle.sh
+++ prog_mode_toggle.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
-status_file=~/.program_mode
-file_contents="$(cat $status_file)"
+status_file="$HOME/.program_mode"
+file_contents="$(cat "$status_file")"
 
 function on {
-    setxkbmap er nodeadkeys &&
-    sleep 0.5 &&
-    xmodmap ~/.xmodmap_prog &&
-    sleep 0.5 &&
-    xcape -e 'Control_R=Escape' &&
-    xset r rate 300 35 && # keyboard repeat speed
-    echo "on" > "$status_file" &&
-    notify-send 'Programming keyboard mode' 'on'
+    (set -e
+     setxkbmap er nodeadkeys
+     sleep 0.5
+     xmodmap "$HOME/.xmodmap_prog"
+     sleep 0.5
+     xcape -e 'Control_R=Escape'
+     xset r rate 300 35 # keyboard repeat speed
+     echo "on" > "$status_file"
+     notify-send 'Programming keyboard mode' 'on'
+    )
 
-    if [[ "$?" -ne 0 ]]; then
-        notify-send 'Programming keyboard mode' 'error :('
+    if [[ $? -ne 0 ]]; then
+        notify-send 'Programming keyboard mode' 'error turning on :('
         exit 1
     fi
 }
@@ -36,38 +38,59 @@ function sweoff {
 }
 
 function off {
-    setxkbmap se
-    killall xcape 2>/dev/null
-    echo "off" > "$status_file" &&
-    notify-send 'Programming keyboard mode' 'off'
+    (set -e
+     setxkbmap se
+     pkill xcape 2>/dev/null
+     echo "off" > "$status_file"
+     notify-send 'Programming keyboard mode' 'off'
+    )
+    if [[ $? -ne 0 ]]; then
+        notify-send 'Programming keyboard mode' 'error turning off :('
+        exit 1
+    fi
 }
 
-if [[ "$1" == "off" ]]; then
-    off
-elif [[ "$1" == "on" ]]; then
-    on
-elif [[ "$1" == "swetoggle" ]]; then
-    if [[ "$file_contents" == "swe" ]]; then
-        sweoff
-    elif [[ "$file_contents" == "on" ]]; then
-        sweon
-    else
-        notify-send 'åäö mode' "program mode is not enabled, can't toggle"
-        exit 1
-    fi
-elif [[ -z "$1" ]]; then
-    if [[ "$file_contents" == "on" ]] || [[ "$file_contents" == "swe" ]]; then
+case "$1" in
+    off)
         off
-    elif [[ "$file_contents" == "off" ]]; then
+        ;;
+    on)
         on
-    else
-        echo "something is wrong with your file" >&2
+        ;;
+    swetoggle)
+        case "$file_contents" in
+            swe)
+                sweoff
+                ;;
+            on)
+                sweon
+                ;;
+            *)
+                notify-send 'åäö mode' "program mode is not enabled, can't toggle"
+                exit 1
+                ;;
+        esac
+        ;;
+    "")
+        case "$file_contents" in
+            on | swe)
+                off
+                ;;
+            off)
+                on
+                ;;
+            *)
+                echo "something is wrong with your file" >&2
+                exit 1
+                ;;
+        esac
+        ;;
+    *)
+        echo "your argument is wrong" >&2
         exit 1
-    fi
-else
-    echo "your argument is wrong" >&2
-    exit 1
-fi
+        ;;
+esac
+
 
 # update polybar module
 pkill --signal SIGUSR1 progmode
