4132e53a94c9dde9404602740a7f4e08203db095
Erik Rimskog <erik.rimskog@gmail.com>
2018-05-29 23:18:07 +0200

    screenshots now actually works

--- i3/scrot_clipboard.sh
+++ i3/scrot_clipboard.sh
@@ -2,12 +2,12 @@
 
 tmp='/tmp/scrot_to_clipboard.png'
 
-if echo "$@" | grep '-s' - >/dev/null; then
+if echo "$@" | grep -- '-s' >/dev/null; then
    notify-send 'scrot_clipboard' 'Select an area with the mouse'
 fi;
-scrot $@ "$tmp" &&
-xclip -selection clipboard -t image/png -i "$tmp" &&
-notify-send 'scrot_clipboard' 'Screenshot now in clipboard' ||
-notify-send 'scrot_clipboard' 'Screenshot failed!'
+scrot "$@" "$tmp" &&
+    xclip -selection clipboard -t image/png -i "$tmp" &&
+    notify-send 'scrot_clipboard' 'Screenshot now in clipboard' ||
+        notify-send 'scrot_clipboard' 'Screenshot failed!'
 
 rm -f "$tmp"
