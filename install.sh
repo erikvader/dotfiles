832e610ffd0be7593ab5c07c518cbf36688ff68a
Erik Rimskog <erik.rimskog@gmail.com>
2018-06-25 00:47:11 +0200

    improved install.sh

--- install.sh
+++ install.sh
@@ -10,12 +10,50 @@ if [[ $1 = -f ]]; then
     force=true
 fi
 
+function colorize {
+    tput bold
+    tput setaf "$1"
+    echo -n "$2"
+    tput sgr0
+}
+
 # wrapper for ln
 function place {
-    if ln -sT ${force:+-f} "$1" "$2"; then
-        printf "%s -> %s\\n" "$2" "$1"
+    if [[ -L "$2" && "$(readlink "$2")" = "$1" ]]; then
+        colorize 2 "EXISTS "
+    elif [[ -z "$force" && -e "$2" ]]; then
+        colorize 1 "EXISTS "
+    else
+        if [[ -n "$force" && -e "$2" ]]; then
+            rm -ir "$2"
+        fi
+        if ln -sT "$1" "$2"; then
+            colorize 2 "OK "
+        else
+            colorize 1 "FAILED "
+        fi
     fi
+    printf "%s l-> %s\\n" "$1" "$2"
 }
+
+# only for files
+function placecp {
+    if [[ -f "$2" && "$(cat "$1")" = "$(cat "$2")" ]]; then
+        colorize 2 "EXISTS "
+    elif [[ -z "$force" && -f "$2" ]]; then
+        echo "File exists: $2" >&2
+        colorize 1 "EXISTS "
+    else
+        if cp -i "$1" "$2"; then
+            colorize 2 "OK "
+        else
+            colorize 1 "FAILED "
+        fi
+    fi
+    printf "%s c-> %s\\n" "$1" "$2"
+}
+
+declares="$(declare -f colorize; declare -f place; declare -f placecp); force=$force"
 
 bin_location="$HOME/.bin"
 scripts=(
@@ -57,9 +95,16 @@ place "$PWD/polybar"       "$HOME/.config/polybar"
 place "$PWD/mpv"           "$HOME/.config/mpv"
 place "$PWD/.compton.conf" "$HOME/.config/compton.conf"
 place "$PWD/redshift.conf" "$HOME/.config/redshift.conf"
+place "$PWD/sxiv/sxiv"     "$HOME/.config/sxiv"
 place "$PWD/zsh/custom"    "$HOME/.my-oh-my-zsh-custom"
 place "$PWD/.profile"      "$HOME/.profile"
 place "$PWD/.xmodmap_prog" "$HOME/.xmodmap_prog"
 place "$PWD/zsh/.zshrc"    "$HOME/.zshrc"
 place "$PWD/makefiles"     "$HOME/makefiles"
+
+# thing in urxvt that is overridden after each update
+if [[ -f "/usr/lib/urxvt/perl/eval" ]]; then
+    # urxvt is installed
+    sudo bash -c "$declares; placecp \"$PWD/urxvt/eval\" \"/usr/lib/urxvt/perl/eval\""
+fi
 
