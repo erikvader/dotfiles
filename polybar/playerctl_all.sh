8cd22510cd2dfe73a2e95ff010abd1b1c84c7c8f
Erik Rimskog <erik.rimskog@gmail.com>
2018-08-21 00:32:16 +0200

    added polybar bar for non-laptop typ

--- polybar/playerctl_all.sh
+++ polybar/playerctl_all.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
-if playerctl -l 2>/dev/null | grep spotify &>/dev/null; then
-    echo "$(playerctl --player=spotify metadata artist) - $(playerctl --player=spotify metadata title)"
-else
-    echo ""
-fi;
+while true; do
+    if playerctl -l 2>/dev/null | grep spotify &>/dev/null; then
+        if [[ "$(playerctl --player=spotify status)" = Playing ]]; then
+            echo -n ' '
+        else
+            echo -n '  '
+        fi
+        playerctl --player=spotify metadata artist
+        echo -n ' - '
+        playerctl --player=spotify metadata title
+        echo
+        sleep 5
+    else
+        echo ""
+        sleep 60
+    fi;
+done
