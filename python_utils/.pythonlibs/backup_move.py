20a95176b02bf6f4c436b4d918adc78871d23231
Erik Rimskog <erik.rimskog@gmail.com>
2019-01-20 01:13:30 +0100

    backup_move traded race conditions for moving directories

--- python_utils/.pythonlibs/backup_move.py
+++ python_utils/.pythonlibs/backup_move.py
@@ -16,8 +16,8 @@ def _file_gen(name):
 
 def move_file(src, dst, src_folder=None):
    """
-   Moves the source file src to the folder dst, renaming src as
-   necessary to make sure nothing in dst gets overwritten.
+   Moves the source file/directory src to the folder dst, renaming src
+   as necessary to make sure nothing in dst gets overwritten.
 
    returns the name (not path) of the file moved to dst.
 
@@ -33,19 +33,11 @@ def move_file(src, dst, src_folder=None):
       src = os.path.basename(src)
 
    src_path = os.path.join(src_folder, src)
-   if not os.path.isfile(src_path):
-      raise MoveFileException("source file doesn't exist or is not a file")
+   if not os.path.exists(src_path):
+      raise MoveFileException("source file doesn't exist")
 
    for d_name in _file_gen(src):
       d_path = os.path.join(dst, d_name)
-      try:
-         # Why this is not a problem:
-         # https://stackoverflow.com/questions/2028874/what-happens-to-an-open-file-handle-on-linux-if-the-pointed-file-gets-moved-del
-         # This is done to prevent race conditions (at least with itself).
-         # Can also use link and unlink because link doesn't overwrite stuff.
-         # Using this because move can copy between file systems.
-         with open(d_path, 'xb'):
-            move(src_path, d_path)
+      if not os.path.exists(d_path):
+         move(src_path, d_path)
          return d_name
-      except FileExistsError:
-         continue
