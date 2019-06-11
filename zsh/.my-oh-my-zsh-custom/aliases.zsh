a1163ec27c18c4e1acf0d8d9ebebc89233b1ef80
Erik Rimskog <erik.rimskog@gmail.com>
2019-06-11 18:44:47 +0200

    fixed stupid bug in yorr

--- zsh/.my-oh-my-zsh-custom/aliases.zsh
+++ zsh/.my-oh-my-zsh-custom/aliases.zsh
@@ -33,7 +33,7 @@ alias yo='yay -Qdt' # list orphans
 alias yor='yay -Rn $(yay -Qqdt)' # remove orphans non-recursively
 function yorr { # remove orphans recursively
     local orphans=($(yay -Qqdt))
-    if [[ "${#orphans[@]}" > 0 ]]; then
+    if [[ "${#orphans[@]}" -gt 0 ]]; then
         yay --noconfirm -Rn "${orphans[@]}"
         yor
     fi
