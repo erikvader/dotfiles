bd0cdab80d22ce33578da8d24aba3fbc5f65ee39
Erik Rimskog <erik.rimskog@gmail.com>
2021-06-09 20:49:34 +0200

    splitext is less buggy

--- python_utils/.pythonlibs/splitext.py
+++ python_utils/.pythonlibs/splitext.py
@@ -2,16 +2,30 @@ import os
 
 def splitext(path):
     """
-    The same as os.path.splitext except this splits filenames like
-    hej.tar.gz properly.
+    The same as os.path.splitext, but handles some double-extensions as one.
 
-    os.path: ('hej.tar', '.gz')
-    this: ('hej', '.tar.gz')
+    Old behaviour:
+    >>> os.path.splitext('hej.tar.gz')
+    ('hej.tar', '.gz')
+
+    New behaviour! If the first extension is from a compression utility, then merge them:
+    >>> splitext('hej.tar.gz')
+    ('hej', '.tar.gz')
+
+    mp4 isn't:
+    >>> splitext('hej.mkv.mp4')
+    ('hej.mkv', '.mp4')
+
+    Also works if the second extension is not .tar:
+    >>> splitext('hej.mkv.gz')
+    ('hej', '.mkv.gz')
     """
     filename, ext = os.path.splitext(path)
-
     if not ext:
         return filename, ext
 
-    act_filename, act_ext = splitext(filename)
-    return act_filename, act_ext + ext
+    filename2, ext2 = os.path.splitext(filename)
+    if ext2 and ext in {".gz", ".bz2", ".xz", ".lzma", ".Z", ".zst", ".lz", ".lzo"}:
+        return filename2, ext2 + ext
+
+    return filename, ext
