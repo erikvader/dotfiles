96718582284a186e0ad020d63f2d719f6c00f995
Erik Rimskog <erik.rimskog@gmail.com>
2021-07-03 21:43:03 +0200

    fifoecho: fix truncation bug and print errors

--- fifoecho/stow_ignore/fifoecho.c
+++ fifoecho/stow_ignore/fifoecho.c
@@ -5,6 +5,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <errno.h>
 
 // Attempt to write args to file (named pipe) in a non-blocking fashion.
 // Args are intepreted in a similar fashion as echo.
@@ -18,16 +19,17 @@ int main(int argc, char *argv[]){
 
     int fifo = open(argv[1], O_NONBLOCK | O_WRONLY | O_CLOEXEC);
     if (fifo < 0) {
+        fprintf(stderr, "could not open: '%s'\n", strerror(errno));
         exit(EXIT_FAILURE);
     }
 
-    ssize_t len = 1;
+    ssize_t len = 1; // newline
     for (int i = 2; i < argc; ++i) {
         len += strlen(argv[i]);
         len += 1;
     }
 
-    char *line = calloc(len+1, sizeof(char));
+    char *line = calloc(len+1, sizeof(char)); // plus null
     assert(line);
     for (int i = 2; i < argc; ++i) {
         strcat(line, argv[i]);
@@ -37,13 +39,17 @@ int main(int argc, char *argv[]){
 
     int estatus = EXIT_SUCCESS;
     if (len > PIPE_BUF) {
+        fprintf(stderr, "line is too long: %ld > %d\n", len, PIPE_BUF);
         estatus = EXIT_FAILURE;
         len = PIPE_BUF;
+        line[len] = '\0';
     }
+    assert(len >= 1); // newline must fit
     line[len-1] = '\n';
 
     ssize_t written = write(fifo, line, len);
     if (written != len) {
+        fprintf(stderr, "could not write: '%s'\n", strerror(errno));
         estatus = EXIT_FAILURE;
     }
 
