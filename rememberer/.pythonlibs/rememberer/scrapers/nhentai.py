1ad3ed2f6fddc8bbe4f5acc19e20c836f3e1d8ed
Erik Rimskog <erik.rimskog@gmail.com>
2022-11-07 18:41:59 +0100

    different coloring in rememberer

--- rememberer/.pythonlibs/rememberer/scrapers/nhentai.py
+++ rememberer/.pythonlibs/rememberer/scrapers/nhentai.py
@@ -2,12 +2,54 @@
 from .scraping import *
 
 
+def get_read_ids():
+    import re
+    import os
+
+    IDFINDER = re.compile(r"^\[?([0-9]+?)( |\])")
+    HENTAIDIR = "/media/NAS/Anime/Manga/nhentai/read"  # TODO: make this configurable
+    s = set()
+
+    if os.path.isdir(HENTAIDIR):
+        with os.scandir(HENTAIDIR) as it:
+            for f in it:
+                if match := IDFINDER.match(f.name):
+                    s.add(match.group(1))
+                else:
+                    print(
+                        "Unexpected filename: {}".format(f.name),
+                        file=sys.stderr,
+                    )
+
+    # TODO: cache this
+    return s
+
+
+def marker_factory(everything: bool):
+    if not everything:
+        return lambda key: "lime"
+    else:
+        read_ids = get_read_ids()
+
+        def mark_color(key):
+            if not read_ids:
+                return "red"
+            elif key in read_ids:
+                return "cyan"
+            else:
+                return "lime"
+
+        return mark_color
+
+
 def nhentaiScraper(
     soup: Soup,
     url: ParseResult,
     everything: bool = False,
     **_kwargs,
 ) -> List[Thing]:
+    color_fun = marker_factory(everything)
+
     def video_key(urlpath: str) -> Optional[str]:
         try:
             test = extract_path(urlpath, 2, 0)
@@ -28,7 +70,9 @@ def nhentaiScraper(
         if not name:
             raise ParseException("title name empty")
 
-        return [Thing(name=name, key=key, jsmark=change_color_on(title, "lime"))]
+        return [
+            Thing(name=name, key=key, jsmark=change_color_on(title, color_fun(key)))
+        ]
 
     def findall() -> List[Thing]:
         if video_key(url.path) is not None:
@@ -45,7 +89,7 @@ def nhentaiScraper(
                 raise ParseException("invalid link in tag")
 
             things.append(
-                Thing(name=name, key=key, jsmark=change_color_on(title, "lime"))
+                Thing(name=name, key=key, jsmark=change_color_on(title, color_fun(key)))
             )
         return things
 
