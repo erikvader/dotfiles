f13816c97c862bface003e82a2b3c7626b6ce621
Erik Rimskog <erik.rimskog@gmail.com>
2022-12-03 21:02:50 +0100

    rememberer: colorize yt

--- rememberer/.pythonlibs/rememberer/scrapers/yt.py
+++ rememberer/.pythonlibs/rememberer/scrapers/yt.py
@@ -2,15 +2,59 @@
 from .scraping import *
 
 
-def YTScraper(soup: Soup, url: ParseResult, **_kwargs) -> List[Thing]:
+def YTScraper(
+    soup: Soup,
+    url: ParseResult,
+    everything: bool = False,
+    **_kwargs,
+) -> List[Thing]:
+    return findall(soup, url) if everything else current_page(soup, url)
+
+
+def video_key_from_url(url: str):
+    return extract_parameter(url, "v")
+
+
+def current_page(soup: Soup, url: ParseResult) -> List[Thing]:
     if not url.path.startswith("/watch"):
         raise UserError("wrong page")
 
-    key = extract_parameter(url.query, "v")
+    key = video_key_from_url(url.query)
 
-    title = soup.select_one("h1.title.ytd-video-primary-info-renderer")
+    title = soup.select_one("#title > h1")
     if not title:
         raise ParseException("couldn't find video title")
     name = scrape_text(title, recursive=True)
 
-    return [Thing(name=name, key=key)]
+    return [
+        Thing(
+            name=name,
+            key=key,
+            jsmark=change_color_on(title, "lime", fuzzy_body=True, style="background"),
+        )
+    ]
+
+
+def findall(soup: Soup, url: ParseResult) -> List[Thing]:
+    things = ignore_user_error(current_page, soup, url)
+
+    others = soup.select("a.yt-simple-endpoint")
+    for video in others:
+        if "href" not in video.attrs:
+            continue
+        href = video["href"]
+        if not href.startswith("/watch?"):
+            continue
+        key = video_key_from_url(href.removeprefix("/watch?"))
+        name = scrape_text(video, recursive=True, allow_empty=True)
+        things.append(
+            Thing(
+                name=name,
+                key=key,
+                jsmark=change_color_on(
+                    video, "lime", fuzzy_body=True, style="background"
+                ),
+            )
+        )
+
+    return things
