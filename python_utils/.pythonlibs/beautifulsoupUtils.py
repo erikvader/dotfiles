98dce8264cd104d1634e6deb70507f4906a1c467
Erik Rimskog <erik.rimskog@gmail.com>
2022-12-03 21:02:07 +0100

    unique_selector: flag for annoying youtube

--- python_utils/.pythonlibs/beautifulsoupUtils.py
+++ python_utils/.pythonlibs/beautifulsoupUtils.py
@@ -2,7 +2,7 @@ from typing import Any, Optional
 from bs4.element import Tag
 
 
-def unique_selector(tag: Any) -> Optional[str]:
+def unique_selector(tag: Any, fuzzy_body: bool = False) -> Optional[str]:
     parents = list(tag.parents)
     if len(parents) <= 2:  # tag must be inside body
         return None
@@ -11,13 +11,21 @@ def unique_selector(tag: Any) -> Optional[str]:
         return None
     path = []
     for child, parent in zip([tag] + parents, parents):
-        children_tags = (c for c in parent.children if isinstance(c, Tag))
-        nth_child = next(
-            (count + 1 for count, c in enumerate(children_tags) if c is child), None
-        )
-        if nth_child is None:
-            return None
-        path.append("{}:nth-child({})".format(child.name, nth_child))
+        # NOTE: Some sites (youtube) have divs and other tags in <head>, which normally
+        # don't belong there. BeautifulSoup 4.11.1 seems to put those tags in <body>
+        # instead, making its child cound incorrect. So the direct child of <body> doesn't
+        # use nth-child. The returned selector is no longer unique though, but probably,
+        # in mose cases.
+        if fuzzy_body and parent.name == "body":
+            path.append(child.name)
+        else:
+            children_tags = (c for c in parent.children if isinstance(c, Tag))
+            nth_child = next(
+                (count + 1 for count, c in enumerate(children_tags) if c is child), None
+            )
+            if nth_child is None:
+                return None
+            path.append("{}:nth-child({})".format(child.name, nth_child))
     path.append("body")
 
     return " > ".join(reversed(path))
