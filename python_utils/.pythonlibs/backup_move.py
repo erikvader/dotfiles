8eeb54f1f4524b28086473e9acc1522275b05d11
Erik Rimskog <erik.rimskog@gmail.com>
2022-12-03 21:01:04 +0100

    backup_move: simplified and return path instead of name

--- python_utils/.pythonlibs/backup_move.py
+++ python_utils/.pythonlibs/backup_move.py
@@ -2,8 +2,6 @@ import os
 import re
 from shutil import move
 
-# TODO: put duplicate number before file extension instead?
-
 _leading_number_regex = re.compile(r"[0-9]+_(.+)")
 
 class MoveFileException(Exception):
@@ -19,37 +17,29 @@ def _file_gen(name):
       yield "{}_{}".format(str(i), name)
       i += 1
 
-def move_file(src, dst, src_folder=None):
+def move_file(src, dst):
    """
    Moves the source file/directory src to the folder dst, renaming src
    as necessary to make sure nothing in dst gets overwritten.
 
-   returns the name (not path) of the file moved to dst.
+   returns the path (not name) of the file moved to dst.
 
-   move_file("path/to/file", "path/to/dst")
-   is equivalent to
-   move_file("file", "path/to/dst", src_folder="path/to")
-
-   dstname = move_file("path/to/file", "path/to/dst")
-   assert(os.path.isfile(os.path.join("path/to/dst", dstname)))
+   dstpath = move_file("path/to/file", "path/to/dst")
    """
-   if not src_folder:
-      src_folder = os.path.dirname(src)
-      src = os.path.basename(src)
-
-   src_path = os.path.join(src_folder, src)
-   if not os.path.exists(src_path):
+   if not os.path.exists(src):
       raise MoveFileException("source file doesn't exist")
 
    if not os.path.isdir(dst):
       raise MoveFileException("dst is not a directory")
 
-   for d_name in _file_gen(src):
+   src_basename = os.path.basename(src)
+
+   for d_name in _file_gen(src_basename):
       d_path = os.path.join(dst, d_name)
-      # NOTE: possible race condition
+      # NOTE: race condition, open with exclusive flag instead
       if not os.path.exists(d_path):
-         move(src_path, d_path)
-         return d_name
+         move(src, d_path)
+         return d_path
 
 # find all possible duplicates of the file `path` in it's current directory
 def find_duplicates(path):
