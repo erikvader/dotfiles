ccb0b9ccc8f11e0f3112c0858c643ebbc8a41600
Erik Rimskog <erik.rimskog@gmail.com>
2023-01-21 22:13:15 +0100

    zsh: fix fzf not working

--- zsh/.zshrc.d/30-ohmyzsh.zsh
+++ zsh/.zshrc.d/30-ohmyzsh.zsh
@@ -3,6 +3,3 @@ source "$HOME"/.local/share/ohmyzsh/lib/git.zsh
 
 # plugin for compact paths in the prompt
 source "$HOME"/.local/share/ohmyzsh/plugins/shrink-path/shrink-path.plugin.zsh
-
-# plugin for smarter history searches and stuff
-source "$HOME"/.local/share/ohmyzsh/plugins/fzf/fzf.plugin.zsh
---
+++ zsh/.zshrc.d/60-ohmyzsh-fzf.zsh
@@ -0,0 +1,4 @@
+# NOTE: something in completions.zsh messes with this, so this needs to be run after it
+
+# plugin for smarter history searches and stuff
+source "$HOME"/.local/share/ohmyzsh/plugins/fzf/fzf.plugin.zsh
