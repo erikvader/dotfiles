684aa05dedfec01078bd4098697ec70b814728b0
Erik Rimskog <erik.rimskog@gmail.com>
2023-12-18 20:09:26 +0100

    fix man colors

--- zsh/.zshrc.d/20-colors.zsh
+++ zsh/.zshrc.d/20-colors.zsh
@@ -17,11 +17,12 @@ alias ls='ls --color=auto'
 alias grep='grep --color=auto'
 
 function man {
-    LESS_TERMCAP_md=$'\e[01;35m' \
-    LESS_TERMCAP_me=$'\e[0m' \
-    LESS_TERMCAP_se=$'\e[0m' \
-    LESS_TERMCAP_so=$'\e[45;30m' \
-    LESS_TERMCAP_ue=$'\e[0m' \
-    LESS_TERMCAP_us=$'\e[01;34m' \
+    LESS_TERMCAP_md=$(tput bold; tput setaf 5) \
+    LESS_TERMCAP_me=$(tput sgr0) \
+    LESS_TERMCAP_so=$(tput setab 5; tput setaf 0) \
+    LESS_TERMCAP_se=$(tput sgr0) \
+    LESS_TERMCAP_us=$(tput bold; tput setaf 4) \
+    LESS_TERMCAP_ue=$(tput sgr0) \
+    GROFF_NO_SGR=1 \
     command man "$@"
 }
