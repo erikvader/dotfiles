2d62d0aa41dcf2f41af7edb8cc1eb299d03b5a8c
Erik Rimskog <erik.rimskog@gmail.com>
2024-08-04 22:12:59 +0200

    rememberer: support for yt shorts

--- rememberer/stow_ignore/rememberer/src/rememberer/scrapers/yt.py
+++ rememberer/stow_ignore/rememberer/src/rememberer/scrapers/yt.py
@@ -15,10 +15,11 @@ def video_key_from_url(url: str):
     return extract_parameter(url, "v")
 
 
-def current_page(soup: Soup, url: ParseResult) -> List[Thing]:
-    if not url.path.startswith("/watch"):
-        raise UserError("wrong page")
+def short_key_from_url(url: str):
+    return extract_path(url, 2, 1)
 
+
+def current_video(soup: Soup, url: ParseResult) -> List[Thing]:
     key = video_key_from_url(url.query)
 
     title = soup.select_one("#title > h1")
@@ -35,6 +36,32 @@ def current_page(soup: Soup, url: ParseResult) -> List[Thing]:
     ]
 
 
+def current_short(soup: Soup, url: ParseResult) -> List[Thing]:
+    key = short_key_from_url(url.path)
+
+    title = soup.select_one("ytd-reel-video-renderer[is-active] h2.title")
+    if not title:
+        raise ParseException("couldn't find video title")
+    name = scrape_text(title, recursive=True)
+
+    return [
+        Thing(
+            name=name,
+            key=key,
+            jsmark=change_color_on(title, "lime", fuzzy_body=True, style="background"),
+        )
+    ]
+
+
+def current_page(soup: Soup, url: ParseResult) -> List[Thing]:
+    if url.path.startswith("/watch"):
+        return current_video(soup, url)
+    elif url.path.startswith("/shorts"):
+        return current_short(soup, url)
+    else:
+        raise UserError("wrong page")
+
+
 def findall(soup: Soup, url: ParseResult) -> List[Thing]:
     things = ignore_user_error(current_page, soup, url)
 
@@ -43,9 +70,14 @@ def findall(soup: Soup, url: ParseResult) -> List[Thing]:
         if "href" not in video.attrs:
             continue
         href = video["href"]
-        if not href.startswith("/watch?"):
+
+        if href.startswith("/watch?"):
+            key = video_key_from_url(href.removeprefix("/watch?"))
+        elif href.startswith("/shorts/"):
+            key = short_key_from_url(href)
+        else:
             continue
-        key = video_key_from_url(href.removeprefix("/watch?"))
+
         name = scrape_text(video, recursive=True, allow_empty=True)
         things.append(
             Thing(
