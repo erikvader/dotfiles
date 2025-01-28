10b07372e656cee9e8644e55a35cb10216c7a5dc
Erik Rimskog <erik.rimskog@gmail.com>
2025-01-29 00:50:51 +0100

    git: delta emulate diff-highlight

--- git/.zshrc.d/50-git.zsh
+++ git/.zshrc.d/50-git.zsh
@@ -3,5 +3,6 @@ GIT_COMPLETION_SHOW_ALL=1
 GIT_COMPLETION_SHOW_ALL_COMMANDS=1
 
 alias g=git
+alias gs='g -c delta.side-by-side=true'
 alias gup='cd "$(git rev-parse --show-cdup)"'
 alias gsuper='cd "$(git rev-parse --show-superproject-working-tree)"'
