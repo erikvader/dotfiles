d2b4bfb551ccb0ff14277f183b04d92f3d57e7e3
Erik Rimskog <erik.rimskog@gmail.com>
2025-10-12 22:33:03 +0200

    zsh: remove expensive elements from prompt

--- zsh/.zshrc.d/30-ohmyzsh.zsh
+++ zsh/.zshrc.d/30-ohmyzsh.zsh
@@ -1,5 +1,1 @@
-# functions used for git information in the prompt
-source "$HOME"/.local/share/ohmyzsh/lib/git.zsh
-
-# plugin for compact paths in the prompt
-source "$HOME"/.local/share/ohmyzsh/plugins/shrink-path/shrink-path.plugin.zsh
+# Intentionally left blank
--- zsh/.zshrc.d/90-theme.zsh
+++ zsh/.zshrc.d/90-theme.zsh
@@ -15,47 +15,36 @@ if [[ $SSH_CLIENT || $SSH_CONNECTION || $SSH_TTY ]]; then
     user_host='%{$terminfo[bold]$fg[yellow]%}%n@%m%{$reset_color%}'
 fi
 
-current_dir='%{$terminfo[bold]$fg[blue]%}$(shrink_path -l -t)%{$reset_color%}'
+# https://stackoverflow.com/a/73590795
+prompt_pwd () {
+    local p=${${PWD:/~/\~}/#~\//\~/}
+    echo -n "${(@j[/]M)${(@s[/]M)p##*/}##(.|)?}$p:t"
+}
 
-# local MODE_INDICATOR="──[%{$fg_bold[yellow]%}NORMAL%{$reset_color%}]"
-# function vi_mode_prompt_info() {
-#   echo "${${KEYMAP/vicmd/$MODE_INDICATOR}/(main|viins)/}"
-# }
+current_dir='%{$terminfo[bold]$fg[blue]%}$(prompt_pwd)%{$reset_color%}'
 
 function ranger_prompt {
-  if [[ "$RANGER_LEVEL" ]]; then
-    echo -n "──[%{$fg[cyan]%}RANGER%{$reset_color%}]"
-  fi
+    if [[ $RANGER_LEVEL ]]; then
+        echo -n "──[%{$fg[cyan]%}RANGER%{$reset_color%}]"
+    fi
 }
 
 function wine_prompt {
-    if export -p | grep -q 'export WINEPREFIX='; then
+    if [[ $WINEPREFIX ]]; then
         echo -n "──[%{$fg[red]%}$WINEPREFIX%{$reset_color%}]"
     fi
 }
 
 function virtualenv_prompt {
-    if [[ -n $VIRTUAL_ENV ]]; then
+    if [[ $VIRTUAL_ENV ]]; then
         echo -n "──[%{$fg[magenta]%}${VIRTUAL_ENV:t}%{$reset_color%}]"
     fi
 }
 
-PROMPT="┌─[${user_host}]──[${current_dir}]"'$(git_prompt_info)$(ranger_prompt)$(wine_prompt)$(virtualenv_prompt)'"
+PROMPT="┌─[${user_host}]──[${current_dir}]"'$(ranger_prompt)$(wine_prompt)$(virtualenv_prompt)'"
 └─%B${user_symbol}%b "
 RPS1="%B${return_code}%b"
 
 unset user_host
 unset user_symbol
 unset current_dir
-
-ZSH_THEME_GIT_PROMPT_PREFIX="──[%{$fg[yellow]%}"
-ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%}]"
-ZSH_THEME_GIT_PROMPT_MODIFIED="M"
-ZSH_THEME_GIT_PROMPT_UNTRACKED="U"
-ZSH_THEME_GIT_PROMPT_ADDED="A"
-ZSH_THEME_GIT_PROMPT_RENAMED="R"
-ZSH_THEME_GIT_PROMPT_DELETED="D"
-ZSH_THEME_GIT_PROMPT_STASHED="S"
-ZSH_THEME_GIT_PROMPT_AHEAD="↑"
-ZSH_THEME_GIT_PROMPT_BEHIND="↓"
-ZSH_THEME_GIT_PROMPT_DIVERGED="↯"
