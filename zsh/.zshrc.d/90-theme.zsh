d8308c731da96f2929c2ecf906b33faecc8eba58
Erik Rimskog <erik.rimskog@gmail.com>
2026-03-22 00:28:55 +0100

    add different fish-path to zsh theme

--- zsh/.zshrc.d/90-theme.zsh
+++ zsh/.zshrc.d/90-theme.zsh
@@ -1,8 +1,6 @@
 # Modified version of https://github.com/ohmyzsh/ohmyzsh/blob/master/themes/bira.zsh-theme
 # ZSH Theme - Preview: http://gyazo.com/8becc8a7ed5ab54a0262a470555c3eed.png
 
-return_code="%(?..%{$fg[red]%}%? ↵%{$reset_color%})"
-
 if [[ $UID -eq 0 ]]; then
     user_host='%{$terminfo[bold]$fg[red]%}%n@%m%{$reset_color%}'
     user_symbol='#'
@@ -15,13 +13,42 @@ if [[ $SSH_CLIENT || $SSH_CONNECTION || $SSH_TTY ]]; then
     user_host='%{$terminfo[bold]$fg[yellow]%}%n@%m%{$reset_color%}'
 fi
 
-# https://stackoverflow.com/a/73590795
-prompt_pwd () {
-    local p=${${PWD:/~/\~}/#~\//\~/}
-    echo -n "${(@j[/]M)${(@s[/]M)p##*/}##(.|)?}$p:t"
+user_symbol="%(?..%{$fg[red]%})${user_symbol}%{$reset_color%}"
+
+# This is a highly condensed version of the plugin provided by oh-my-zsh
+# https://github.com/ohmyzsh/ohmyzsh/blob/master/plugins/shrink-path/README.md
+# I created my own because the oh-my-zsh version always expands a glob on every directory
+# in the PWD, effectively running ls on them every time the prompt is displayed, which I
+# don't want. This version is dumber and shortens the path simply by string manipulations.
+function shrink_path {
+    typeset -i lastfull=2
+    typeset -i complen=2
+
+    typeset -a components
+    typeset result dir=${1-$PWD}
+
+    dir=${dir/#$HOME/\~}
+    components=(${(s:/:)dir})
+
+    if [[ $components[1] == \~* ]]; then
+       result=$components[1]
+       shift components
+    fi
+
+    for comp in $components; do
+        if (( $#components <= lastfull )); then
+            result+=/$comp
+        elif [[ $comp == .* ]]; then
+            result+=/$comp[1,(( complen+1 ))]
+        else
+            result+=/$comp[1,$complen]
+        fi
+        shift components
+    done
+    echo ${result:-/}
 }
 
-current_dir='%{$terminfo[bold]$fg[blue]%}$(prompt_pwd)%{$reset_color%}'
+current_dir='%{$terminfo[bold]$fg[blue]%}$(shrink_path)%{$reset_color%}'
 
 function ranger_prompt {
     if [[ $RANGER_LEVEL ]]; then
@@ -43,7 +70,6 @@ function virtualenv_prompt {
 
 PROMPT="┌─[${user_host}]──[${current_dir}]"'$(ranger_prompt)$(wine_prompt)$(virtualenv_prompt)'"
 └─%B${user_symbol}%b "
-RPS1="%B${return_code}%b"
 
 unset user_host
 unset user_symbol
