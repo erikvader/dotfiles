bade481b9ba6f2dd849f3c30a9a7796ca3df2a54
Erik Rimskog <erik.rimskog@gmail.com>
2026-03-31 23:31:26 +0200

    zsh: use zsh instead of tput

--- zsh/.zshrc.d/20-colors.zsh
+++ zsh/.zshrc.d/20-colors.zsh
@@ -17,12 +17,12 @@ alias ls='ls --color=auto'
 alias grep='grep --color=auto'
 
 function man {
-    LESS_TERMCAP_md=$(tput bold; tput setaf 5) \
-    LESS_TERMCAP_me=$(tput sgr0) \
-    LESS_TERMCAP_so=$(tput setab 5; tput setaf 0) \
-    LESS_TERMCAP_se=$(tput sgr0) \
-    LESS_TERMCAP_us=$(tput bold; tput setaf 4) \
-    LESS_TERMCAP_ue=$(tput sgr0) \
+    LESS_TERMCAP_md=$fg_bold[magenta] \
+    LESS_TERMCAP_me=$reset_color \
+    LESS_TERMCAP_so=$bg[magenta]$fg[black] \
+    LESS_TERMCAP_se=$reset_color \
+    LESS_TERMCAP_us=$fg_bold[blue] \
+    LESS_TERMCAP_ue=$reset_color \
     GROFF_NO_SGR=1 \
     command man "$@"
 }
