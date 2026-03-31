196946ffe69d9073acca679a648d98e5d0f82ab0
Erik Rimskog <erik.rimskog@gmail.com>
2026-03-31 23:32:13 +0200

    split .gitconfig into several files

--- git/.zshrc.d/50-git.zsh
+++ git/.zshrc.d/50-git.zsh
@@ -5,6 +5,7 @@ GIT_COMPLETION_SHOW_ALL_COMMANDS=1
 # TODO: this could be a custom command that accepts a fuzzy search for the submodule to
 # operate on. Perhaps integrate that in git-foreach instead?
 alias g=git
+# TODO: use DELTA_FEATURES instead?
 alias gg='g -c delta.side-by-side=true'
 alias gup='cd "$(git rev-parse --show-cdup)"'
 alias gsuper='cd "$(git rev-parse --show-superproject-working-tree)"'
