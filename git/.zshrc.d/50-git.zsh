bd44ceff1d626a5f310e04e82dfcca69b58583af
Erik Rimskog <erik.rimskog@gmail.com>
2026-05-01 22:55:46 +0200

    git: submodule cd

--- git/.zshrc.d/50-git.zsh
+++ git/.zshrc.d/50-git.zsh
@@ -1,11 +1,17 @@
+# NOTE: this file should work in bash as well
+
 # Show more things when autocompleting git
 GIT_COMPLETION_SHOW_ALL=1
 GIT_COMPLETION_SHOW_ALL_COMMANDS=1
 
-# TODO: this could be a custom command that accepts a fuzzy search for the submodule to
-# operate on. Perhaps integrate that in git-foreach instead?
 alias g=git
-alias gup='cd "$(git rev-parse --show-cdup)"'
-alias gsuper='cd "$(git rev-parse --show-superproject-working-tree)"'
 
-# TODO: en cd som bara autocompletar submoduler
+gcd() {
+    local p
+    if p=$(xgit-submodule-cd "$@"); then
+        cd "$p"
+    else
+        return 1
+    fi
+
+}
